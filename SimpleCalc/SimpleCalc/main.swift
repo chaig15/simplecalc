dc9eff70d2f2c4a811996436018169110ca81378
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-01-14 16:05:12 -0800

    passed all tests

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,53 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var operation: String = "";
+        var nums = [Int]();
+        for string in args {
+            let num = Int(string);
+            if (num != nil) {
+                nums.append(num!);
+            } else {
+                operation = string;
+            }
+        }
+        if (operation == "+") {
+            return nums[0] + nums[1];
+        } else if (operation == "-") {
+            return nums[0] - nums[1];
+        }  else if (operation == "/") {
+            return (nums[0]/nums[1]);
+        } else if (operation == "%") {
+            return (nums[0]%nums[1]);
+        } else if (operation == "*") {
+            return (nums[0]*nums[1]);
+        } else if (operation == "count") {
+            return nums.count;
+        } else if (operation == "avg") {
+            if (nums.count == 0) {
+                return 0;
+            }
+            var sum: Int = 0;
+            for num in nums {
+                sum += num;
+            }
+            return sum/nums.count;
+        } else if (operation == "fact") {
+            if (nums.count == 0) {
+                return 0;
+            } else if (nums.count == 1 && nums[0] == 1 || nums[0] == 0) {
+                return 1;
+            }
+            var fact: Int = 1;
+            var num = nums[0];
+            while num > 0 {
+                fact = fact * num;
+                num = num - 1;
+            }
+            return fact;
+        }
+        return 0;
+        
     }
     
     public func calculate(_ arg: String) -> Int {
